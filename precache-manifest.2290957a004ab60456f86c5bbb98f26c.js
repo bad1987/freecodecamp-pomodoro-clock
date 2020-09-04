361e44793208d4b88df37864144dfe9a7630c3d2
badinfo1987 <badinfo1987@gmail.com>
2020-09-04 12:03:07 +0100

    Updates

---
+++ precache-manifest.2290957a004ab60456f86c5bbb98f26c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660695a1587e91086abf38f97bfdefd6",
+    "url": "/freecodecamp-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "a80259c0b22423878c34",
+    "url": "/freecodecamp-pomodoro-clock/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b54ca82bb46c18e67e9b",
+    "url": "/freecodecamp-pomodoro-clock/static/css/main.e60f645d.chunk.css"
+  },
+  {
+    "revision": "a80259c0b22423878c34",
+    "url": "/freecodecamp-pomodoro-clock/static/js/2.c44b689c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/freecodecamp-pomodoro-clock/static/js/2.c44b689c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54ca82bb46c18e67e9b",
+    "url": "/freecodecamp-pomodoro-clock/static/js/main.cf4c84d1.chunk.js"
+  },
+  {
+    "revision": "a62cda448cb3516af905",
+    "url": "/freecodecamp-pomodoro-clock/static/js/runtime-main.b6dcb22a.js"
+  },
+  {
+    "revision": "54a4aa0750507aa6ea8cd7c8895b0a40",
+    "url": "/freecodecamp-pomodoro-clock/static/media/alarm.54a4aa07.mp3"
+  }
+]);
